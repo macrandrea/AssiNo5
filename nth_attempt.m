a647e61a8872bf1970af01a723c48810e4e0bee8
macrandrea <macri.andr@gmail.com>
2022-02-17 00:00:07 +0100

    mancano le wolfe

--- nth_attempt.m
+++ nth_attempt.m
@@ -7,15 +7,13 @@ x=ones(maxit,2);
 l=ones(maxit,3);
 z=ones(maxit+1,3);
 mu=20;
-x(1,1:2)=[1.2,0.44];
+x(1,1:2)=[1.9,0.44];
 l(1,1:3)=[2,3,4];
 z(1,1:3)=[1,3,4];
 I=eye(3);
 A=[-1,-1;1,0;0,1];
 H=[2 , 0;0 , 2,];
-passoX=1;
-passoZ=1;
-passoL=1;
+passoX=[0.001,0.01]';
 e=[1,1,1];
 f(1)=(x(1,1)-4)^2-x(1,2)^2+mu*e*log(z(1,1:3))';
 for i=1:maxit
@@ -32,30 +30,20 @@ for i=1:maxit
     deltaX=(H+A'*(Z\L)*A)\(-r(i,1:2)'-A'*(Z\r(i,3:5)'+L\r(i,6:8)'));
     deltaL=(L\Z)\(-A*deltaX-r(i,3:5)'+L\r(i,6:8)');
     deltaZ=-L\(r(i,6:8)'+Z*deltaL);
-    slX=deltaX'*(-r(i,1:2)'-A'*(Z\r(i,3:5)'+L\r(i,6:8)'));
-    slZ=deltaZ'*((r(i,6:8)'+Z*deltaL));
-    if (x(i,1)+x(i,2)>2)%x(i,1)>0 && x(i,2)>0 && x(i,1)<2 && x(i,2)<2
-        
-       x(i+1,:)=x(i,:)+passoX*deltaX';
-       l(i+1,:)=l(i,:)+passoL*deltaL';
-       z(i+1,:)=z(i,:)+passoZ*deltaZ'; 
-       f(i+1)=(x(i+1,1)-4)^2-(x(i+1,2))^2-mu*e*log(z(i+1,:))';                
-       f(i)=(x(i,1)-4)^2-(x(i,2))^2-mu*e*log(z(i,:))';
-                   if(f(i+1)>f(i)-0.1*1*[slX',slZ']')
-                        passoX=passoX/2;
-                        passoZ=passoZ/2;
-                   else
-                        break
-                   end
-    elseif (x(i,1)<0 || x(i,2)<0)
-        x(i,1)=-x(i,1)+2;
-        x(i,2)=-x(i,2)+2;
-        passoX=passoX*0.1;
-    end 
-   if(norm(r(i,:))>tol)
-       mu=mu/2;
-   else
-      break;
-   end
+    slX=deltaX'*r(i,1:2)';
+    %if(norm(r(i,:))>tol)
+        x(i+1,:)=x(i,:)+0.01*deltaX';
+        l(i+1,:)=l(i,:)+0.01*deltaL';
+        z(i+1,:)=z(i,:)+0.01*deltaZ';  
+        f(i+1)=(x(i+1,1)-4)^2-(x(i+1,2))^2-mu*e*log(z(i+1,:))';
+        if(f(i+1)>f(i))
+            mu=mu/2;
+        else
+            break
+        end
+    if(norm(r(i,:))>tol)
+        mu=mu/2;
+    else
+       break;
+    end
 end
-%end
